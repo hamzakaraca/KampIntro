90d2ee622b15b933601ee5898d00e2ca62230be0
hamzakaraca <79093890+hamzakaraca@users.noreply.github.com>
2021-02-24 15:33:19 +0300

    2.hafta
    2.hafta

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kurs kurs1=new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Eğitmen = "Engin Demiroğ";
+            kurs1.İzlenmeOrani = 68;
+            
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Eğitmen { get; set; }
+        public int İzlenmeOrani { get; set; }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Eşittir butonu");
+            }
+
+
+
+
+            if (sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
