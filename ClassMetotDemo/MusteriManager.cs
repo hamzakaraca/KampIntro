6c0e2376ead091ac717faf2305500a490104442e
hamzakaraca <79093890+hamzakaraca@users.noreply.github.com>
2021-02-26 12:00:55 +0300

    ödev 3

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri) 
+        {
+            Console.WriteLine( "Müşteri eklendi: " + musteri.Adi + musteri.SoyAdi);
+        }
+        public void MusteriListele(Musteri musteri,Musteri musteri1)
+        {
+            Musteri[] musteriler = new Musteri[] {musteri,musteri1 };
+            foreach (Musteri a in musteriler)
+            {
+                Console.WriteLine(a.Id);
+                Console.WriteLine(a.Adi);
+                Console.WriteLine(a.SoyAdi);
+
+            }
+            
+        }
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi: " + musteri.Adi + musteri.SoyAdi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Musteri musteri1= new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Eyüp";
+            musteri1.SoyAdi = "Metinoğlu";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.SoyAdi = "Karaca";
+            musteri2.Adi = "Savaş";
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriListele(musteri1,musteri2);
+            
+        }
+    }
+}
